efd14f2b8f1df07e0197fe96bd7653c27847a317
Justin Shapcott <support@mobidevelop.com>
2013-09-27 21:29:43 -0700

    Add copyright notice

--- maps-basic/src/com/mobidevelop/maps/basic/objects/BasicPolygonMapObject.java
+++ maps-basic/src/com/mobidevelop/maps/basic/objects/BasicPolygonMapObject.java
@@ -1,3 +1,19 @@
+/*******************************************************************************
+ * Copyright 2013 See AUTHORS File
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ******************************************************************************/
+
 package com.mobidevelop.maps.basic.objects;
 
 import com.badlogic.gdx.math.Polygon;
@@ -16,7 +32,12 @@ public class BasicPolygonMapObject extends BasicMapObject implements PolygonMapO
 	public void setVertices(float[] vertices) {
 		polygon.setVertices(vertices);
 	}
-	
+
+	@Override
+	public float[] getTransformedVertices() {
+		return this.polygon.getTransformedVertices();
+	}
+
 	public BasicPolygonMapObject(MapLayer layer) {
 		super(layer);
 		this.polygon = new Polygon();
